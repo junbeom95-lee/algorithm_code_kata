a20f80e3b8c28bc1c541b3758fdebb54dcb49afd
junbeom95-lee <junbeom95@gmail.com>
2025-10-03 22:56:26 +0900

    Algorithm Code Kata 003 solution

---
+++ src/CodeKata003.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CodeKata003 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            float mul = (float)num1 / num2;
+            answer = (int)(mul * 1000);
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
+        CodeKata003 solution = new CodeKata003();
+        int answer = solution.new Solution().solution(num1, num2);
+        System.out.println(answer);
+    }
+}
