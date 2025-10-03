99b36693332647a7393e8f12122cbd5f721555c8
junbeom95-lee <junbeom95@gmail.com>
2025-10-03 23:15:55 +0900

    Algorithm Code Kata 009 soltion

---
+++ src/CodeKata009.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CodeKata009 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i=i+2) answer += i;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        CodeKata009 solution = new CodeKata009();
+        int answer = solution.solution(n);
+        System.out.println(answer);
+    }
+}
