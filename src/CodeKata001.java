1a2aea159355fa06c3c83eb919fff5b2b2280daa
junbeom95-lee <junbeom95@gmail.com>
2025-10-03 22:24:48 +0900

    Algorithm Code Kata 001 solution

---
+++ src/CodeKata001.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CodeKata001 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
+        CodeKata001 solution = new CodeKata001();
+        int answer = solution.solution(num1, num2);
+        System.out.println(answer);
+    }
+}
