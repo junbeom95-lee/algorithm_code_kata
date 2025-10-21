953246e706a3ecfb706914cbdcf44131f2251c56
junbeom95-lee <junbeom95@gmail.com>
2025-10-21 10:48:39 +0900

    Algorithm Code Kata 034 solution

---
+++ src/code_030/CodeKata034.java
@@ -0,0 +1,20 @@
+package code_030;
+
+import java.util.Arrays;
+
+public class CodeKata034 {  //문자열 내림차순으로 배치하기
+    public String solution(String s) {
+        String answer = "";
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);   //내림차순 정렬
+        answer = new StringBuilder(new String(arr)).reverse().toString();   //역순
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CodeKata034 c = new CodeKata034();
+        String test = "Zbcdefg";
+        String result = c.solution(test);
+        System.out.println("result = " + result);
+    }
+}
